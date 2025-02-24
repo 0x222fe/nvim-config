d7c39b7f0aef126569f595cc3b1633441ad71203
Jason Wong <42604000+0x222fe@users.noreply.github.com>
2025-02-24 13:23:07 +0800

    add Gvdiff keymap

--- lua/plugins/fugitive.lua
+++ lua/plugins/fugitive.lua
@@ -3,11 +3,11 @@ return {
 	config = function()
 		vim.keymap.set("n", "<leader>gf", vim.cmd.Git, { desc = "[G]it [F]ugitive" })
 
-		local ThePrimeagen_Fugitive = vim.api.nvim_create_augroup("ThePrimeagen_Fugitive", {})
+		local Fugitive = vim.api.nvim_create_augroup("Fugitive", {})
 
 		local autocmd = vim.api.nvim_create_autocmd
 		autocmd("BufWinEnter", {
-			group = ThePrimeagen_Fugitive,
+			group = Fugitive,
 			pattern = "*",
 			callback = function()
 				if vim.bo.ft ~= "fugitive" then
@@ -22,14 +22,11 @@ return {
 
 				-- rebase always
 				vim.keymap.set("n", "<leader>P", function()
-					vim.cmd.Git({ "pull", "--rebase" })
+					vim.cmd.Git({ "pull" })
 				end, opts)
-
-				-- NOTE: It allows me to easily set the branch i am pushing and any tracking
-				-- needed if i did not set the branch up correctly
-				vim.keymap.set("n", "<leader>t", ":Git push -u origin ", opts)
 			end,
 		})
+		vim.keymap.set("n", "<leader>gd", vim.cmd.Gvdiff, { desc = "[G]it [D]iff" })
 
 		vim.keymap.set("n", "gu", "<cmd>diffget //2<CR>")
 		vim.keymap.set("n", "gh", "<cmd>diffget //3<CR>")
