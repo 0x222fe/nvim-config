805dc491c0c967d2f4b0ac38d20267e683491407
Jason Wong <42604000+0x222fe@users.noreply.github.com>
2025-10-21 14:56:13 +0800

    _

---
+++ lua/lsp/harper.lua
@@ -0,0 +1,33 @@
+vim.lsp.config("harper_ls", {
+	settings = {
+		["harper-ls"] = {
+			userDictPath = "",
+			workspaceDictPath = "",
+			fileDictPath = "",
+			linters = {
+				SpellCheck = true,
+				SpelledNumbers = false,
+				AnA = true,
+				SentenceCapitalization = true,
+				UnclosedQuotes = true,
+				WrongQuotes = true,
+				LongSentences = true,
+				RepeatedWords = true,
+				Spaces = true,
+				Matcher = true,
+				CorrectNumberSuffix = true,
+			},
+			codeActions = {
+				ForceStable = false,
+			},
+			markdown = {
+				IgnoreLinkTitle = false,
+			},
+			diagnosticSeverity = "hint",
+			isolateEnglish = false,
+			dialect = "American",
+			maxFileLength = 120000,
+			ignoredLintsPath = {},
+		},
+	},
+})
