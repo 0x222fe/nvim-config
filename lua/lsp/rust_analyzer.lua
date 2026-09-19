5b0e6fb8ebdf0f5e29ef9c97e05d3909d6a80468
Jason Wong <42604000+0x222fe@users.noreply.github.com>
2026-09-19 22:48:42 +0800

    _

--- lua/lsp/rust_analyzer.lua
+++ lua/lsp/rust_analyzer.lua
@@ -1,7 +1,11 @@
 vim.lsp.config("rust_analyzer", {
     settings = {
         ["rust-analyzer"] = {
-            checkOnSave = true,
+            -- checkOnSave = true,
+            check = {
+                enable = true,
+                command = "clippy",
+            },
             procMacro = {
                 enable = true,
             },
